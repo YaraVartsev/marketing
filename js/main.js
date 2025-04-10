31144e120def16e7e9b7b63020439a5184575776
Yaroslav Vartsev <futbik1990@gmail.com>
2025-04-10 15:02:54 +0300

    arrowAdd

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ $(function(){
   
   $('.slider-blog-inner').slick({
     dots: true, 
-    prevArrow: '<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="arrow-left"></button>', 
-    nextArrow: '<button type="button" class="slick-next"><img src="../images/arrow-right.svg" alt="arrow-right"></button>', 
+    prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="arrow-left"></button>', 
+    nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="arrow-right"></button>', 
    });
 
 });
