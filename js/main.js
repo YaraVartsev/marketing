a7badb605dfa7e93097b985a6759497864117efd
Yaroslav Vartsev <futbik1990@gmail.com>
2025-04-14 15:11:30 +0300

    headerAdaptive

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-// $(function () {
-  
-//   $('.slider').slick({
-//     arrows: false,
-//     dots: true
-//   });
 
 
 
-// });
-
-
-$(function(){
-  var mixer = mixitup('.portfolio__content');
+$(function () {
   
   $('.slider-blog-inner').slick({
     dots: true, 
     prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="arrow-left"></button>', 
     nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="arrow-right"></button>', 
-   });
+  });
+  
+  $('.menu__btn').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active')
+  });
+
+
+
+  var mixer = mixitup('.portfolio__content');
 
 });
