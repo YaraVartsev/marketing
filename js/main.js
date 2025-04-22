18b4dbbe75240e097add8d325907e5b8f244ab77
Yaroslav Vartsev <futbik1990@gmail.com>
2025-04-22 22:05:32 +0300

    made an adaptive

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ $(function () {
     dots: true, 
     prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="arrow-left"></button>', 
     nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right.svg" alt="arrow-right"></button>', 
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      },
+    ]
   });
   
   $('.menu__btn').on('click', function () {
